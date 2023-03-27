eef534053501ba85302d9fc19da35961fcec63ea
leejeoungeun <80578194+leejeoungeun@users.noreply.github.com>
2023-03-27 23:28:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let display = document.getElementById('display');
+let input = '0';
 
-function addToDisplay(value) {
-	display.value += value;
+function addNumber(num) {
+  if (input === '0') {
+    input = num;
+  } else {
+    input += num;
+  }
+  updateDisplay(input);
 }
 
-function clearDisplay() {
-	display.value = '';
+function addOperator(operator) {
+  input += operator;
+  updateDisplay(input);
+}
+
+function clearInput() {
+  input = '0';
+  updateDisplay(input);
 }
 
 function calculate() {
-	try {
-		display.value = eval(display.value);
-	} catch (error) {
-		display.value = 'Error';
-	}
+  let result = eval(input);
+  input = result.toString();
+  updateDisplay(input);
 }
 
+function updateDisplay(input) {
+  document.getElementById('result').value = input;
+}
